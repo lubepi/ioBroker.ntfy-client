8f62a39dde49c72c6df435cae5a6e30c6d0b1b3b
lubepi <lukas.piel@gmail.com>
2026-03-20 19:41:58 +0100

    chore: remove deprecated unit tests

--- test/unit.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const { tests } = require("@iobroker/testing");
-
-// Run unit tests - See https://github.com/ioBroker/testing for more detailed usage.
-tests.unit(path.join(__dirname, ".."));
